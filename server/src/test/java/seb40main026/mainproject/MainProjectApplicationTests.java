cf55ef7e400a86e780c2b56be18278f1afc5d048
oasis791 <khwi791@naver.com>
2022-11-11 10:36:26 +0900

    :tada: init: Server Initial Commit

---
+++ server/src/test/java/seb40main026/mainproject/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package seb40main026.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
