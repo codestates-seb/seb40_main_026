772b72b16e6da6bbe094ba1e5d15fd9deaf992c4
oasis791 <khwi791@naver.com>
2022-11-17 03:31:35 +0900

    :sparkles: feat: Exception Code 추가

--- server/src/main/java/seb40main026/mainproject/exception/ExceptionCode.java
+++ server/src/main/java/seb40main026/mainproject/exception/ExceptionCode.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 public enum ExceptionCode {
 
     //주어진 상황에 맞게 enum 을 추가해  BusinessException을 사용하면 됩니다.
-    BOAST_NOT_FOUND(404,"Boast Not Found");
-
+    BOAST_NOT_FOUND(404,"Boast Not Found"),
+    MEMBER_NOT_FOUND(404, "Member Not Found"),
+    MEMBER_EXISTS(409, "Member exists");
     @Getter
     private int status;
 
