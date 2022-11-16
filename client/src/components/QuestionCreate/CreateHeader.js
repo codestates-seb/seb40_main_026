e5b13f3c1bf30c0400855f7ed2250593deca8f20
kjs0508 <kss0866@gmail.com>
2022-11-16 14:26:58 +0900

    ✨feat:질문 작성페이지 구현

---
+++ client/src/components/QuestionCreate/CreateHeader.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+const Container = styled.header`
+  margin-bottom: 1rem;
+  width: 100%;
+  margin: auto;
+  margin-top: 1rem;
+  .TitleWrap {
+    width: 70%;
+    color: #ffc149;
+    margin: auto;
+    padding-bottom: 1rem;
+    border-bottom: 2px solid #ffc149;
+  }
+`;
+const CreateHeader = () => {
+  return (
+    <>
+      <Container>
+        <div className="TitleWrap">
+          <h1>질문하기</h1>
+        </div>
+      </Container>
+    </>
+  );
+};
+export default CreateHeader;
