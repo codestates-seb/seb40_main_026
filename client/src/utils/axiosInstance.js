29c07dd6b423c06b4366ad7585209bbdcb7ee057
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-17 10:50:59 +0900

     :truck:rename: utils 생성

---
+++ client/src/utils/axiosInstance.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use();
+
+axiosInstance.interceptors.response.use();
+
+export default axiosInstance;
