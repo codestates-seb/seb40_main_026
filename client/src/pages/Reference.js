850d72975df61154fdd6b62a51922d9da44d1e05
hyeleeeeen <heo22969087@gmail.com>
2022-11-18 11:27:43 +0900

     :truck:rename : 모여봐요 페이지 생성

---
+++ client/src/pages/Reference.js
@@ -0,0 +1,12 @@
+// import styled from 'styled-components';
+import Card from '../components/Boast/Card';
+
+function Reference() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default Reference;
