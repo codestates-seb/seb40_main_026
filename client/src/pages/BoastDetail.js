42837ec2078358e34dc457bcbebbd3fe7b3a0b90
hyeleeeeen <heo22969087@gmail.com>
2022-11-18 13:05:45 +0900

    :sparkle: feat : 자랑하기상세페이지 구현

--- client/src/pages/BoastDetail.js
+++ client/src/pages/BoastDetail.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { tablet } from '../styles/Responsive';
-import BoastView from '../components/Boast/BoastView';
+import QuestionView from '../components/QuestionView/QuestionView';
+import Commentlist from '../components/Shared/Commentlist';
+import CommentCreate from '../components/Shared/CommentCreate';
 
 const Container = styled.main``;
 
@@ -15,7 +17,9 @@ function BoastDetail() {
   return (
     <Container>
       <Title>자랑할래요</Title>
-      <BoastView />
+      <QuestionView />
+      <CommentCreate />
+      <Commentlist />
     </Container>
   );
 }
