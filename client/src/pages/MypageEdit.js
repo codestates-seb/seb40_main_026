5dbd00e420e0c4b67028102fe6d3e00c0687e626
kjs0508 <kss0866@gmail.com>
2022-11-23 14:50:08 +0900

     :sparkle: feat:Mypage 및 회원정보 수정 페이지 구현

---
+++ client/src/pages/MypageEdit.js
@@ -0,0 +1,10 @@
+import MypageEditContainer from '../components/Mypage/MypageEdit';
+
+const MypageEdit = () => {
+  return (
+    <>
+      <MypageEditContainer />
+    </>
+  );
+};
+export default MypageEdit;
