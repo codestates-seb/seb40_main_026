61e849fcaec21cc6c564bc6e9341b4fcaacc6bd6
hyeleeeeen <heo22969087@gmail.com>
2022-11-24 14:37:31 +0900

    :art :style : 자랑하기 상세페이지 css 수정

--- client/src/pages/BoastDetail.js
+++ client/src/pages/BoastDetail.js
@@ -1,28 +1,30 @@
 import styled from 'styled-components';
-import { tablet } from '../styles/Responsive';
-import QuestionView from '../components/QuestionView/QuestionView';
+import DetailView from '../components/Shared/DetailView';
 import Commentlist from '../components/Shared/Commentlist';
 import CommentCreate from '../components/Shared/CommentCreate';
 
 const Container = styled.main``;
 
-const Title = styled.h1`
-  margin: 50px 0px 0px 100px;
-  margin-bottom: 60px;
-  @media ${tablet} {
-    font-size: 25px;
-  }
-`;
-
-const CommentBox = styled.section`
-  margin-top: 100px;
-`;
+const CommentBox = styled.section``;
 
 function BoastDetail() {
+  const DummyQuestions = {
+    id: 1,
+    title: '안녕하세요 제목을 입력해주세요',
+    body: '안녕하세요 내용을 입력해주세요안녕하세요 내용을 입력해주세요안녕하세요 내용을 입력해주세요안녕하세요 내용을 입력해주세요',
+    date: '22-11-14',
+    nickname: '파닥몬',
+    grade: '질문왕',
+    class: '🐣',
+    likeCount: '3',
+    answerlength: '2',
+  };
   return (
     <Container>
-      <Title>자랑할래요</Title>
-      <QuestionView />
+      <DetailView
+        DummyData={DummyQuestions}
+        likeCount={DummyQuestions.likeCount}
+      />
       <CommentBox>
         <CommentCreate />
         <Commentlist />
