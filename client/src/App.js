4b7373aa9fcf009ebe7fa644ae3f9c52007da070
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-24 20:47:19 +0900

     :sparkle: feat: App.js 리프레시 토큰 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,49 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Footer from './components/layout/Footer/Footer';
 import Header from './components/layout/Header/Header';
 import Mypage from './components/Mypage/Mypage';
+import MypageEdit from './components/Mypage/MypageEdit';
+import useRefreshToken from './hooks/useRefreshToken';
 import Boast from './pages/Boast';
 import BoastCreate from './pages/BoastCreate';
-import LoginPage from './pages/Loginpage';
+import LoginPage from './pages/LoginPage';
+import QuestionCreate from './pages/QuestionCreate';
+import QuestionsMain from './pages/Questionlist';
+import QuestionViewMain from './pages/QuestionView';
 import SignupPage from './pages/SignupPage';
 import StudyListPage from './pages/StudyListPage';
 import StudyViewPage from './pages/StudyViewPage';
 import GlobalStyle from './styles/GlobalStyle';
-import QuestionsMain from './pages/Questionlist';
-import QuestionViewMain from './pages/QuestionView';
-import QuestionCreate from './pages/QuestionCreate';
-import MypageEdit from './components/Mypage/MypageEdit';
+
 function App() {
+  const refresh = useRefreshToken();
+
+  useEffect(() => {
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    verifyRefreshToken();
+  }, [refresh]);
+
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <ToastContainer
+        position="top-center"
+        className={'toast'}
+        autoClose="2000"
+      />
       <Header />
       <Routes>
         <Route path="/" element={<div>홈</div>} />
-        <Route path="/signup" element={<div>회원가입</div>} />
-        <Route path="/login" element={<div>로그인</div>} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/mypage/edit" element={<MypageEdit />} />
         <Route path="/questions" element={<QuestionsMain />} />
