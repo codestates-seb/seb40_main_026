a63a9dd36cf78f45ce47d330b11a2c89516a4ba4
oasis791 <khwi791@naver.com>
2022-11-25 17:34:00 +0900

    :bug: fix: cors 설정 변경

--- server/src/main/java/seb40main026/mainproject/config/SecurityConfig.java
+++ server/src/main/java/seb40main026/mainproject/config/SecurityConfig.java
@@ -93,6 +93,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
