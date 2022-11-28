96b76b303da77e1190c61013490cd1f028adb0b3
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-28 11:46:28 +0900

    :bug: fix: 카카오맵 수정

---
+++ client/src/components/StudyView/Map.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import KakaoMapScript from './KakaoMapScript';
+
+export default function Map() {
+  useEffect(() => {
+    KakaoMapScript();
+  }, []);
+
+  return (
+    <div
+      id="myMap"
+      style={{
+        width: '1000px',
+        height: '500px',
+      }}
+    ></div>
+  );
+}
