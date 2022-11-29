645070698b39b1af9eb8c90401b293637b50e6b8
kjs0508 <kss0866@gmail.com>
2022-11-29 16:41:21 +0900

    :bug: fix:랜더링 오류 수정

--- client/src/components/QuestionView/QuestionView.js
+++ client/src/components/QuestionView/QuestionView.js
@@ -18,7 +18,6 @@ const QuestionView = ({
   const token = localStorage.getItem('accessToken');
   const navigate = useNavigate();
 
-  console.log(State);
   //최초 질문 상세페이지 조회시
   useEffect(() => {
     axios({
@@ -31,7 +30,7 @@ const QuestionView = ({
       SetQuesData(res.data);
       console.log(res.data);
     });
-  }, [State, ContentData]);
+  }, [State]);
   //질문 수정
   const EditPatch = () => {
     axios({
@@ -45,6 +44,23 @@ const QuestionView = ({
       .catch((err) => {
         console.log(err);
       });
+  };
+  const ReportHandler = () => {
+    axios({
+      method: 'post',
+      url: `http://ec2-3-34-95-255.ap-northeast-2.compute.amazonaws.com:8080/questions/${id}/report`,
+      data: { id },
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then(function (response) {
+        SetState(State + 1);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    window.alert('신고가 완료 되었습니다.');
   };
   const DeleteHandler = () => {
     axios
@@ -84,8 +100,7 @@ const QuestionView = ({
         EditPatch={EditPatch}
         DeleteHandler={DeleteHandler}
         LikeHandler={LikeHandler}
-        SetState={SetState}
-        State={State}
+        ReportHandler={ReportHandler}
       />
     </>
   );
