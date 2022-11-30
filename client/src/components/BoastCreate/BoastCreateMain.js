ab8a271e015f4a727142a94c883a9caf900e02ec
hyeleeeeen <heo22969087@gmail.com>
2022-11-30 16:53:17 +0900

     :sparkle: feat: 서버데이터와 연결

--- client/src/components/BoastCreate/BoastCreateMain.js
+++ client/src/components/BoastCreate/BoastCreateMain.js
@@ -1,12 +1,46 @@
 import Create from '../Create/Create';
-import BoastCreateHeader from './BoastCreateHeader';
+import TitleHeader from '../Shared/TitleHeader';
 import styled from 'styled-components';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+import { BASE_URL } from '../../utils/api';
+
 const Container = styled.header``;
 const BoastCreate = () => {
+  const [content, SetContent] = useState();
+  const [title, SetTitle] = useState();
+  const access = localStorage.getItem('accessToken');
+  const navigate = useNavigate();
+
+  const Boastpost = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        `${BASE_URL}boasts`,
+        { title, content },
+        { headers: { Authorization: access } }
+      )
+      .then((res) => {
+        console.log(res);
+        navigate('/boast');
+      })
+      .catch((err) => {
+        console.error(err.response.data);
+      });
+  };
+
   return (
     <Container>
-      <BoastCreateHeader />
-      <Create />;
+      <TitleHeader title={'자랑하기'} />
+      <Create
+        PostHandler={Boastpost}
+        SetContent={SetContent}
+        content={content}
+        title={title}
+        SetTitle={SetTitle}
+      />
+      ;
     </Container>
   );
 };
