f1c3b7c45f8cca59860ea64e1e2c4e8ef47161a5
hyeleeeeen <heo22969087@gmail.com>
2022-11-30 16:55:18 +0900

    :bug: fix: 서버데이터 연결 오류 수정

--- client/src/components/Boast/BoastViewMain.js
+++ client/src/components/Boast/BoastViewMain.js
@@ -6,60 +6,50 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { BASE_URL } from '../../utils/api';
 
 function BoastViewMain() {
-  let { boastId } = useParams();
+  let { id } = useParams();
   const [TitleData, SetTitleData] = useState();
   const [ContentData, SetContentData] = useState();
-  const [list, setList] = useState([]);
-  const token = localStorage.getItem('accessToken');
+  const [list, SetList] = useState([]);
+  const access = localStorage.getItem('accessToken');
   const navigate = useNavigate();
 
   // 조회요청
   useEffect(() => {
-    async function fetchItem() {
-      const res = await axios.get(`${BASE_URL}boasts/${boastId}`, {
-        Authorization: token,
-      });
-      let data = res.data;
-      setList(data);
-      console.log(data);
-    }
-    try {
-      fetchItem();
-    } catch (err) {
-      console.error(err);
-    }
+    axios({
+      method: 'get',
+      url: `${BASE_URL}boasts/${id}`,
+    }).then((res) => {
+      SetList(res.data);
+      console.log(res.data);
+    });
   }, []);
 
-  // 수정요청
-
-  //useEffect(() => {
-  // async function EditPatch() {
-  //  const res = await axios.patch(
-  //    `${BASE_URL}boasts/${boastId}`,
-  //   {
-  //     title: TitleData,
-  //     content: ContentData,
-  //  },
-  //   { Authorization: token }
-  // );
-  //  }
-  //  try {
-  //   EditPatch();
-  // } catch (err) {
-  //   console.error(err);
-  //  }
-  //}, []);
+  // 수정 요청
+  const EditPatch = () => {
+    axios({
+      method: 'patch',
+      url: `${BASE_URL}boasts/${id}`,
+      data: { title: TitleData, content: ContentData },
+    })
+      .then(function (response) {
+        EditPatch(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // 삭제요청
 
   const DeleteHandler = () => {
     axios
-      .delete(`${BASE_URL}boasts/${boastId}`, {
+      .delete(`${BASE_URL}boasts/${id}`, {
         headers: {
-          Authorization: token,
+          Authorization: access,
         },
       })
       .then((res) => {
+        console.log(res);
         navigate('/boast');
       });
   };
@@ -71,8 +61,9 @@ function BoastViewMain() {
         SetTitleData={SetTitleData}
         SetContentData={SetContentData}
         DeleteHandler={DeleteHandler}
+        EditPatch={EditPatch}
       />
-      <BoastComment boastId={boastId} />
+      <BoastComment boastId={id} />
     </>
   );
 }
