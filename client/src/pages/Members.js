a2789e7b1849b6c29ca1a57e604c8f6b38a72698
hyeleeeeen <heo22969087@gmail.com>
2022-12-03 22:45:09 +0900

    :recycle:refactor : api 변경으로 인한 코드 삭제,:art: style: 헤더 높이 맞추기

--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import LevelBox from '../components/Members/LevelBox';
 import QuestionBox from '../components/Members/QuestionBox';
 import AnswerBox from '../components/Members/AnswerBox';
@@ -19,7 +17,7 @@ const ListBox = styled.div`
   grid-gap: 2rem;
   margin: auto;
   width: 70%;
-
+  margin-top: 2rem;
   @media ${tablet} {
     display: grid;
     grid-template-columns: 1fr;
@@ -31,30 +29,13 @@ const ListBox = styled.div`
 `;
 
 function Members() {
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    async function getAllMembers() {
-      const res = await axios.get(
-        'http://ec2-3-34-95-255.ap-northeast-2.compute.amazonaws.com:8080/members'
-      );
-      let data = res.data;
-      setList(data);
-      console.log(data);
-    }
-    try {
-      getAllMembers();
-    } catch (err) {
-      console.error(err);
-    }
-  }, []);
-
   return (
     <Container>
       <TitleHeader title={'친구들'} />
       <ListBox>
-        <LevelBox list={list} />
-        <QuestionBox list={list} />
-        <AnswerBox list={list} />
+        <LevelBox />
+        <QuestionBox />
+        <AnswerBox />
       </ListBox>
     </Container>
   );
